8dee2025d4996c8be9621480557b6b0851e1806f
Reza Naghibi <reza.naghibi@yahoo.com>
2015-10-07 13:45:17 -0400

    moved db chooser into java

--- src/main/java/company/api/settings/BaseAppConfig.java
+++ src/main/java/company/api/settings/BaseAppConfig.java
@@ -2,19 +2,28 @@ package company.api.settings;
 
 import company.api.filters.GeneralFilter;
 import company.api.storage.DBStorageManager;
+import company.api.storage.dbrouting.ThreadDBChooser;
 import company.api.storage.dbclients.Database;
 import company.api.utils.Utilities;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import java.util.HashMap;
+import java.util.Map;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  *
  * @author Reza Naghibi
  */
 @Configuration
+@EnableTransactionManagement
 public class BaseAppConfig {
 
   @Bean
@@ -79,5 +88,23 @@ public class BaseAppConfig {
     ds.setIdleConnectionTestPeriod(300);
 
     return ds;
+  }
+
+  @Bean(initMethod = "init")
+  public ThreadDBChooser shardedDataSource(DataSource dataSource_apidb, DataSource dataSource_apishard0,
+          DataSource dataSource_apishard1, FileSettings fileSettings) {
+    ThreadDBChooser dbChooser = new ThreadDBChooser();
+
+    Map<Object, Object> sources = new HashMap<>();
+
+    sources.put("apidb", dataSource_apidb);
+    sources.put("apishard0", dataSource_apishard0);
+    sources.put("apishard1", dataSource_apishard1);
+
+    dbChooser.setTargetDataSources(sources);
+    dbChooser.setDefaultTargetDataSource(dataSource_apidb);
+    dbChooser.setUpdateSchemaDBs(fileSettings.getProperty("db.updateSchema"));
+
+    return dbChooser;
   }
 }
